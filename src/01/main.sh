221a80f9ed77db74995ca84f4f2815d88cd139fa
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2023-01-05 21:23:49 +0300

    Add files via upload

---
+++ src/01/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# == 1 ]]; then
+    value=$1
+    if [[ $value =~ [^0-9] ]]; then
+        echo "$value"
+    else
+        echo "Error, invalid input."
+    fi
+else
+    echo "Error, invalid number of arguments."
+fi
